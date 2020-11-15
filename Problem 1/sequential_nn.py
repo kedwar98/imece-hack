f1e97aef2a7da6fb6559a007f96b1ff21483edbf
Lyle Regenwetter <regenwet@mit.edu>
2020-11-15 11:10:25 -0500

    updates to NN

--- Problem 1/sequential_nn.py
+++ Problem 1/sequential_nn.py
@@ -13,20 +13,25 @@ from tensorflow import keras as k
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn import preprocessing
-
+import tensorflow_addons as tfa
 # if file is in same working directory as data, you can just use the file name
-data=pd.read_csv('resdf.csv', index_col=0)
+data=pd.read_csv('machine0df.csv', index_col=0)
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(data.values)
 data = pd.DataFrame(x_scaled, columns=data.columns,index=data.index.values)
 #we have two target values, damage in x and y 
-xtarget=data['Machines > Bridgeport Mill 1 > Spindle > X-Axial > Damage Accumulation']
-ytarget=data['Machines > Bridgeport Mill 1 > Spindle > Y-Radial > Damage Accumulation']
+# xtarget=data['Machines > Bridgeport Mill 1 > Spindle > X-Axial > Damage Accumulation']
+# ytarget=data['Machines > Bridgeport Mill 1 > Spindle > Y-Radial > Damage Accumulation']
 
+xtarget=data[data.columns[1]]
+ytarget=data[data.columns[11]]
 target=pd.concat([xtarget,ytarget],axis=1)
 
 #create features dataframe by dropping the target columns
-features=data.drop(['Machines > Bridgeport Mill 1 > Spindle > Y-Radial > Damage Accumulation','Machines > Bridgeport Mill 1 > Spindle > X-Axial > Damage Accumulation'],axis=1)
+# features=data.drop(['Machines > Bridgeport Mill 1 > Spindle > Y-Radial > Damage Accumulation','Machines > Bridgeport Mill 1 > Spindle > X-Axial > Damage Accumulation'],axis=1)
+features=data.drop(data.columns[11],axis=1)
+features=features.drop(features.columns[1],axis=1)
+
 model = Sequential()
 model.add(k.layers.BatchNormalization())
 model.add(Dense(128, input_dim=11, activation='selu'))     #CHANGE INPUT_DIM to number of features
@@ -37,12 +42,22 @@ model.add(Dense(2))   #output predicted xdamage and ydamage, both stored in targ
 
 #Adam optimizer is used for preliminary results. Other optimizers may be more stable/robust, perhaps less efficient
 #Use Keras MSLE metric
-model.compile(optimizer='adam',loss=tf.keras.losses.MeanSquaredError()) #I think this is MeanSquaredError function
+# model.compile(optimizer='adam',loss=tf.keras.losses.MeanSquaredError()) #I think this is MeanSquaredError function
 
+def custom_loss(labels, predictions):
+    # result = tfa.metrics.RSquare()
+    # result.update_state(y_true, y_pred)
+    # return result
+    unexplained_error = tf.reduce_sum(tf.square(labels - predictions))
+    total_error = tf.reduce_sum(tf.square(labels - tf.reduce_mean(labels, axis=0)))
+    R2 = 1. - tf.math.divide(unexplained_error, total_error)
+    return R2
+model.compile(optimizer='adam',loss='mse')
+# model.compile(optimizer='adam',loss=custom_loss)
 #Adjust number of epochs (iterations over set), validation split, other arguments, and verbosity
 #Change input as needed
 
-history = model.fit(features, target, validation_split=0.2, epochs=1000,verbose=0)
+history = model.fit(features, target, validation_split=0.2, epochs=100,verbose=0)
 
 
 model.summary()
@@ -65,8 +80,8 @@ evaltest = model.evaluate(features,target,batch_size=1)
 
 #Plot loss over epochs
 
-plt.plot(history.history['val_loss'],label= 'Test loss')
-plt.plot(history.history['loss'],label= 'Train loss')
+plt.plot(history.history['val_loss'][10:],label= 'Test loss')
+plt.plot(history.history['loss'][10:],label= 'Train loss')
 plt.show()
 
 
